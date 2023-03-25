37550e1b9883e35220f1f6bd3d54ff82a1549c7d
John Jolly <jjolly@cs.utah.edu>
2023-03-24 22:48:38 -0600

    Update tui to use the room generator

--- game.py
+++ game.py
@@ -1,23 +1,32 @@
+import room
+import creatures
+
 class Game:
     def __init__(self):
-        self.state = (0, 0)
-        self.board = (40, 20)
+        self.pc = creatures.pc()
+        self.room = room.room(self.pc)
 
     def getBoard(self):
-        board = ['+' + '=' * self.board[0] + '+'] + ['|' + ' ' * self.board[0] + '|'] * self.board[1] + ['+' + '=' * self.board[0] + '+']
-        x = self.state[0]
-        y = self.state[1]
+        x = self.pc.x
+        y = self.pc.y
+        w = self.room.x
+        h = self.room.y
+        board = ['+' + '=' * w + '+'] + ['|' + ' ' * w + '|'] * h + ['+' + '=' * w + '+']
         line = board[y + 1]
         line = line[:x+1] + '#' + line[x+2:]
         board[y + 1] = line
         return board
 
     def sendCommand(self, cmd):
-        if cmd == "RIGHT" and self.state[0] < self.board[0] - 1:
-            self.state = (self.state[0] + 1, self.state[1])
-        if cmd == "LEFT" and self.state[0] > 0:
-            self.state = (self.state[0] - 1, self.state[1])
-        if cmd == "DOWN" and self.state[1] < self.board[1] - 1:
-            self.state = (self.state[0], self.state[1] + 1)
-        if cmd == "UP" and self.state[1] > 0:
-            self.state = (self.state[0], self.state[1] - 1)
+        x = self.pc.x
+        y = self.pc.y
+        w = self.room.x
+        h = self.room.y
+        if cmd == "RIGHT" and x < w - 1:
+           self.pc.setX(x + 1)
+        if cmd == "LEFT" and x > 0:
+           self.pc.setX(x - 1)
+        if cmd == "DOWN" and y < h - 1:
+           self.pc.sety(y + 1)
+        if cmd == "UP" and y > 0:
+           self.pc.setY(y - 1)
