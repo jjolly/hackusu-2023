9d2550a6f5f9d4e3a8bf8fed66c1a6116a84eabb
John Jolly <jjolly@cs.utah.edu>
2023-03-25 11:19:34 -0600

    Add character direction

--- game.py
+++ game.py
@@ -28,6 +28,7 @@ class Game:
         self.event['mondead'] = False
         self.event['itemname'] = ''
         self.event['used'] = ''
+        self.dir = '^'
 
     def getBoard(self):
         x = self.pc.x
@@ -35,7 +36,7 @@ class Game:
         w = self.room.x
         h = self.room.y
         board = ['+' + '=' * w + '+'] + ['|' + ' ' * w + '|'] * h + ['+' + '=' * w + '+']
-        setCharAt(board, x, y, '#')
+        setCharAt(board, x, y, self.dir)
         for ob in self.room.getPillars():
             setCharAt(board, ob[0], ob[1], '*')
         for mon in self.room.getMonsters():
@@ -88,13 +89,17 @@ class Game:
                 self.event['used'] = inv[inum - 1].getName()
                 del inv[inum - 1];
         if cmd == "RIGHT" and x < w - 1:
-           x += 1
+            self.dir = '>'
+            x += 1
         if cmd == "LEFT" and x > 0:
-           x -= 1
+            self.dir = '<'
+            x -= 1
         if cmd == "DOWN" and y < h - 1:
-           y += 1
+            self.dir = 'v'
+            y += 1
         if cmd == "UP" and y > 0:
-           y -= 1
+            self.dir = '^'
+            y -= 1
         for ob in self.room.getPillars():
             if ob[0] == x and ob[1] == y:
                 x = ox
