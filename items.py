e11712cae7c53373b065634195442134ca876ba2
Jazack <31787452+Jazack@users.noreply.github.com>
2023-03-24 21:18:09 -0600

    updating items
    changes allow for random item generation, as well as items healing and hurting the player

--- items.py
+++ items.py
@@ -4,7 +4,16 @@ class: items
 it needs: type
 
 '''
-
+import random
+class createItems:
+    def __init__(self, depth, ID, x, y):
+        itemOptions = [
+            items,
+            healthPotion,
+            woundingPotion,
+            ]
+        do = randint(0, len(itemOptions))
+        return itemOptions[do](depth, ID, x, y)
 class items:
     def __init__(self, depth, ID, x, y):
         self.x = x
@@ -15,3 +24,19 @@ class items:
         return self.id
     def getName(self):
         return self.name
+    def useItem(self, pc):
+        return False
+class healthPotion(items):
+    def __init__(self, depth, ID, x, y):
+        super().__init__(depth, ID, x, y)
+        self.name = 'Potion'
+        self.change = random.randint(depth, 2*(depth+1))
+    def useItem(self, pc):
+        pc.wound(-self.change)
+class woundingPotion(items):
+    def __init__(self, depth, ID, x, y):
+        super().__init__(depth, ID, x, y)
+        self.name = 'Potion'
+        self.change = random.randint(depth, 2*(depth+1))
+    def useItem(self, pc):
+        pc.wound(self.change)
